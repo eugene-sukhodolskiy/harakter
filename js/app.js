a4e3eae78a05b35616f734cff35c376f266b6c97
Eugene <goldmikki@gmail.com>
2020-02-26 10:21:36 +0200

    Make search field
    Issue https://github.com/eugene-sukhodolskiy/harakter-html/issues/7
    
    > * [x]  Search field
    > * [ ]  Search result

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
 	console.log("App Ready");
+	
+	$('.search-bar .go-search').on('blur', function(){
+		$(this).removeClass('active');
+		$(this).val('');
+		$('.search-bar .clear-search-field.show').removeClass('show');
+	});
+
+	$('.search-bar .go-search').on('focus', function(){
+		$(this).addClass('active');
+	});
+
+	$('.search-bar .go-search').on('input', function(){
+		if($(this).val().length){
+			$('.search-bar .clear-search-field').addClass('show');
+		}else{
+			$('.search-bar .clear-search-field.show').removeClass('show');
+		}
+	});
 });
